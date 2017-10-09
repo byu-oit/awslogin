d3a6f0671c44195f39a0793242c2ab9ce1f2dd04
Nathan Peterson <ndpete@gmail.com>
2017-10-09 12:07:06 -0600

    added some cli tests

--- test/byu_awslogin/test_index.py
+++ test/byu_awslogin/test_index.py
@@ -1,5 +1,27 @@
+import pytest
 from byu_awslogin import index
+from click.testing import CliRunner
+from unittest.mock import patch
+from byu_awslogin.util.consoleeffects import Colors
 
 
+@pytest.mark.skip("Not Fully Tested")
 def test_cli():
     print("I NEED TO BE TESTED!")
+
+
+@patch('byu_awslogin.index.get_status')
+def test_cli_status(mock_status):
+    results = CliRunner().invoke(index.cli, ['-s'])
+
+    assert results.exit_code == 0
+    assert mock_status.call_count == 1
+
+
+@patch('byu_awslogin.index.remove_cached_adfs_auth')
+def test_cli_logout(mock_remove_adfs):
+    results = CliRunner().invoke(index.cli, ['--logout'])
+
+    assert results.exit_code == 0
+    assert results.output.strip() == "{}Terminated ADFS Session{}".format(Colors.yellow, Colors.normal)
+    assert mock_remove_adfs.call_count == 1
