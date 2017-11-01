02b386e10a10869769b9e84a5f603cf6116f9572
Nathan Peterson <ndpete@gmail.com>
2017-11-01 14:07:30 -0600

    missed some python3 stuff and removed version pinning

--- setup.py
+++ setup.py
@@ -1,21 +1,10 @@
 #!/usr/bin/env python
-
-from __future__ import unicode_literals
-from __future__ import print_function
-from __future__ import division
-from __future__ import absolute_import
-from builtins import open
-from future import standard_library
-standard_library.install_aliases()
 import os
 import sys
 import subprocess
 from setuptools import setup, find_packages
 from byu_awslogin import index
 
-if not sys.version.startswith('3.6'):
-    sys.stderr.write("byu_awslogin requires python 3.6\n")
-    sys.exit(-1)
 
 with open("README.rst") as rm_file:
     long_description = rm_file.read()
